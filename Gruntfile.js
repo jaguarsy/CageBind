4b8f3ee91c8074d24ff425f436ab3e1b000d8057
JohnnyCage <shangyu@cheyaoshi.com>
2015-09-11 20:23:26 +0800

    Add grunt.

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+/**
+ * Created by johnnycage on 15/9/11.
+ */
+module.exports = function (grunt) {
+    'use strict';
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        banner: '/* LogWeb v<%= pkg.version %> */\n',
+
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            build: [
+                'gruntfile.js',
+                'src/*.js'
+            ],
+        },
+
+        uglify: {
+            options: {
+                banner: '<%= banner %>',
+                mangle: true
+            },
+            build: {
+                files: {
+                    'dist/jquery.cagebind.min.js': 'src/cagebind.js'
+                }
+            }
+        },
+
+        watch: {
+            styles: {
+                files: ['src/**', '*.html', 'Gruntfile.js'],
+                tasks: ['jshint', 'uglify']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['jshint', 'uglify', 'watch']);
+};
